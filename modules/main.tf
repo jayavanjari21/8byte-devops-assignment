a8b400b3541d35910c40cf85010767c4f84bf00f
jayavanjari21 <jayavanjari2101@gmail.com>
2026-02-07 23:40:56 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -52,8 +52,8 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "app" {
-  ami                    = "ami-03f4878755434977f"
-  instance_type          = "t2.micro"
+  ami                    = "ami-0c398cb65a93047f2"
+  instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public.id
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.sg.id]
